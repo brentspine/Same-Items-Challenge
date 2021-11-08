189076c6f9794147049baede1f3fef560c428654
youbo <stolze.luca@gmail.com>
2021-11-08 03:10:32 +0100

    BossBarManager shows Lives after Game start

--- src/java/de/brentspine/sameitems/listeners/PlayerObtainItemListener.java
+++ src/java/de/brentspine/sameitems/listeners/PlayerObtainItemListener.java
@@ -1,14 +1,20 @@
 package de.brentspine.sameitems.listeners;
 
 import de.brentspine.sameitems.Main;
+import de.brentspine.sameitems.util.BossBarManager;
 import de.brentspine.sameitems.util.SettingsManager;
 import org.bukkit.Bukkit;
+import org.bukkit.GameMode;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityPickupItemEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 import java.util.HashMap;
 import java.util.UUID;
@@ -20,6 +26,7 @@ public class PlayerObtainItemListener implements Listener {
 
     @EventHandler
     public void onItemPickup(EntityPickupItemEvent event) {
+        BossBarManager.run();
         if(!(event.getEntity() instanceof Player)) {
             return;
         }
@@ -32,9 +39,15 @@ public class PlayerObtainItemListener implements Listener {
                 return;
             }
             player.sendMessage(obtainedItems.get(event.getItem().getItemStack().getType()));
-            player.sendMessage(Main.PREFIX + "Das Item §9" + event.getItem().getItemStack().getType() + "§7 wurde bereits eingesammelt von §9" + obtainedItems.get(event.getItem().getItemStack().getType()) + " §4(-♥)");
+            player.sendMessage(Main.PREFIX + "Das Item §9" + event.getItem().getItemStack().getType() + "§7 wurde bereits eingesammelt von §9" + obtainedItems.get(event.getItem().getItemStack().getType()) + " §4§l(-♥)");
             lives.put(player.getName(), lives.get(player.getName()) - 1);
-            player.sendMessage(lives.get(player.getName()) + "");
+            player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_DEATH, 1, 1);
+            if(lives.get(player.getName()) <= 0) {
+                player.setHealth(0);
+                for(Player current : Bukkit.getOnlinePlayers()) {
+                    current.sendMessage(Main.PREFIX + "§9" + player.getName() + "§7 ist gestorben §9(Keine Leben mehr)");
+                }
+            }
             return;
         }
         obtainedItems.put(event.getItem().getItemStack().getType(), player.getName());
@@ -53,6 +66,23 @@ public class PlayerObtainItemListener implements Listener {
         if(Main.instance.getTimer().isRunning() && !lives.containsKey(event.getPlayer().getName())) {
             lives.put(event.getPlayer().getName(), SettingsManager.startLives);
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        BossBarManager.bars.remove(event.getPlayer().getName());
+    }
+
+    @EventHandler
+    public void onPlayerDeath(PlayerDeathEvent event) {
+        for(Player current : Bukkit.getOnlinePlayers()) {
+            current.sendMessage(Main.PREFIX + "§9" + event.getEntity().getPlayer().getName() + "§7 ist gestorben §9(Keine Leben mehr)");
+        }
+    }
+
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent event) {
+        event.getPlayer().setGameMode(GameMode.SPECTATOR);
     }
 
 }
--- src/java/de/brentspine/sameitems/util/BossBarManager.java
+++ src/java/de/brentspine/sameitems/util/BossBarManager.java
@@ -1,7 +1,51 @@
 package de.brentspine.sameitems.util;
 
+import de.brentspine.sameitems.Main;
+import de.brentspine.sameitems.listeners.PlayerObtainItemListener;
+import net.md_5.bungee.api.ChatColor;
+import net.md_5.bungee.api.ChatMessageType;
+import net.md_5.bungee.api.chat.TextComponent;
+import org.bukkit.Bukkit;
+import org.bukkit.boss.BarColor;
+import org.bukkit.boss.BarFlag;
+import org.bukkit.boss.BarStyle;
+import org.bukkit.boss.BossBar;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.HashMap;
+
 public class BossBarManager {
 
-    
+    public static HashMap<String, BossBar> bars = new HashMap<>();
+
+    public static void run() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if(!Main.instance.getTimer().isRunning()) {
+                    return;
+                }
+                for(Player player : Bukkit.getOnlinePlayers()) {
+                    if(!bars.containsKey(player.getName())) {
+                        BossBar bar = Bukkit.getServer().createBossBar("§4§l" + PlayerObtainItemListener.lives.get(player.getName()) + "♥", BarColor.BLUE, BarStyle.SOLID, BarFlag.PLAY_BOSS_MUSIC);
+                        bar.setVisible(true);
+                        bar.setProgress(1.0);
+                        bar.addPlayer(player);
+                        String title = "§4§l";
+                        for (int i = 0; i < PlayerObtainItemListener.lives.get(player.getName()); i++) {
+                            title = title + "♥";
+                        }
+                        bars.put(player.getName(), bar);
+                    }
+                    String title = "§4§l";
+                    for (int i = 0; i < PlayerObtainItemListener.lives.get(player.getName()); i++) {
+                        title = title + "♥";
+                    }
+                    bars.get(player.getName()).setTitle(title);
+                }
+            }
+        }.runTaskTimer(Main.instance, 20, 20);
+    }
 
 }
